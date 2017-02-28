873cc763b131b711cecd428791eb04f0d19d3ae6
YangFan0304 <15010958171@163.com>
2017-02-28 15:12:12 +0800

    布局跳转

---
+++ app/src/main/java/com/b1502/store2/activity/CollectActivity.java
@@ -0,0 +1,15 @@
+package com.b1502.store2.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.b1502.store2.R;
+
+public class CollectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect);
+    }
+}
--- app/src/main/java/com/b1502/store2/fragment/UserInfoFragment.java
+++ app/src/main/java/com/b1502/store2/fragment/UserInfoFragment.java
@@ -1,13 +1,25 @@
 package com.b1502.store2.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 import com.b1502.store2.R;
+import com.b1502.store2.activity.AccountActivity;
+import com.b1502.store2.activity.AddActivity;
+import com.b1502.store2.activity.AppraiseActivity;
+import com.b1502.store2.activity.CollectActivity;
+import com.b1502.store2.activity.CouponActivity;
+import com.b1502.store2.activity.OrderActivity;
+import com.b1502.store2.activity.PointsActivity;
+import com.b1502.store2.activity.QuitActivity;
 
 /**
  * 用户信息
@@ -15,7 +27,7 @@ import com.b1502.store2.R;
  * Use the {@link UserInfoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UserInfoFragment extends BaseFragment {
+public class UserInfoFragment extends BaseFragment implements RadioGroup.OnCheckedChangeListener, View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,6 +36,18 @@ public class UserInfoFragment extends BaseFragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+//    private View view;
+    private RadioButton rb_appraise;
+    private RadioButton rb_delivery;
+    private RadioButton rb_order;
+    private RadioButton rb_pay;
+    private RelativeLayout mycollect;
+    private RelativeLayout mycoupon;
+    private RelativeLayout mypoints;
+    private RelativeLayout accountsecurity;
+    private RelativeLayout quit;
+    private RadioGroup rg;
+    private RelativeLayout myadd;
 
 
     public UserInfoFragment() {
@@ -61,7 +85,84 @@ public class UserInfoFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false);
+         View view = inflater.inflate(R.layout.fragment_user_info,null);
+        //获取控件
+        //intoView();
+        rb_appraise = (RadioButton) view.findViewById(R.id.rb_appraise);
+        rb_delivery = (RadioButton) view.findViewById(R.id.rb_delivery);
+        rb_order = (RadioButton) view.findViewById(R.id.rb_order);
+        rb_pay = (RadioButton) view.findViewById(R.id.rb_pay);
+        rg = (RadioGroup) view.findViewById(R.id.rg);
+
+        //RelativeLayout
+        mycollect = (RelativeLayout) view.findViewById(R.id.user_info_mycollect);
+        mycoupon = (RelativeLayout) view.findViewById(R.id.user_info_mycoupon);
+        myadd = (RelativeLayout) view.findViewById(R.id.user_info_myadd);
+        mypoints = (RelativeLayout) view.findViewById(R.id.user_info_mypoints);
+        accountsecurity = (RelativeLayout) view.findViewById(R.id.user_info_accountsecurity);
+        quit = (RelativeLayout) view.findViewById(R.id.user_info_quit);
+        //点击
+        rg.setOnCheckedChangeListener(this);
+        myadd.setOnClickListener(this);
+        mycollect.setOnClickListener(this);
+        mycoupon.setOnClickListener(this);
+        mypoints.setOnClickListener(this);
+        accountsecurity.setOnClickListener(this);
+        quit.setOnClickListener(this);
+
+
+        return view;
     }
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.rb_order:
+                Intent intent_order = new Intent(getActivity(), OrderActivity.class);
+                startActivity(intent_order);
+                break;
+            case R.id.rb_pay:
+                Intent intent_pay = new Intent(getActivity(), OrderActivity.class);
+                startActivity(intent_pay);
+                break;
+            case R.id.rb_delivery:
+                Intent intent_delivery = new Intent(getActivity(), OrderActivity.class);
+                startActivity(intent_delivery);
+                break;
+            case R.id.rb_appraise:
+                Intent intent_appraise = new Intent(getActivity(), AppraiseActivity.class);
+                startActivity(intent_appraise);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.user_info_myadd:
+                Intent intent_myadd = new Intent(getActivity(), AddActivity.class);
+                startActivity(intent_myadd);
+                break;
+            case R.id.user_info_mycollect:
+                Intent intent_mycollect = new Intent(getActivity(), CollectActivity.class);
+                startActivity(intent_mycollect);
+                break;
+            case R.id.user_info_mypoints:
+                Intent intent_mypoints = new Intent(getActivity(), PointsActivity.class);
+                startActivity(intent_mypoints);
+                break;
+            case R.id.user_info_mycoupon:
+                Intent intent_mycoupon = new Intent(getActivity(), CouponActivity.class);
+                startActivity(intent_mycoupon);
+                break;
+            case R.id.user_info_accountsecurity:
+                Intent intent_accountsecurity = new Intent(getActivity(), AccountActivity.class);
+                startActivity(intent_accountsecurity);
+                break;
+            case R.id.user_info_quit:
+                Intent intent_quit = new Intent(getActivity(), QuitActivity.class);
+                startActivity(intent_quit);
+                break;
+        }
+    }
 }
