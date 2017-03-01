4ea737f0a9d3c282678fa092ec6fdd3c7e3dbd11
YangFan0304 <15010958171@163.com>
2017-03-01 17:03:26 +0800

    界面修改

--- app/src/main/java/com/b1502/store2/activity/PointsActivity.java
+++ app/src/main/java/com/b1502/store2/activity/PointsActivity.java
@@ -1,15 +1,23 @@
 package com.b1502.store2.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.b1502.store2.R;
 
-public class PointsActivity extends AppCompatActivity {
+public class PointsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
+        initgetIntent();
+    }
+
+    //接受值
+    private void initgetIntent() {
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        initStoreBar(name);
     }
 }
