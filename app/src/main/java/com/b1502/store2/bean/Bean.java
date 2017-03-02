76479ae1b2d39a00c8a6c748c067a645a98d9535
lenovo <2502554434@qq.com>
2017-03-02 11:34:04 +0800

    张海生提交页面

---
+++ app/src/main/java/com/b1502/store2/bean/Bean.java
@@ -0,0 +1,66 @@
+package com.b1502.store2.bean;
+
+/**
+ * 1.类的用途
+ * 2.@author:zhanghaisheng
+ * 3.@2017/3/1
+ */
+
+
+public class Bean {
+
+
+    /**
+     * Id : 648cf203-16f0-4603-b291-40864c472356
+     * Title : 全球最漂亮餐食
+     * Content : <p><br/></p><p>这位妈妈，你确定这不是在作画吗？</p><p>娇俏美人鱼，小腊肠竖放是可爱章鱼，横放就是呆萌螃蟹<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099710908496093909191_243_211.png" title="blob.png" alt="blob.png"/></p><p>维尼一家三口，岁月安稳，睡姿撩人<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099716306152345963158.png" title="blob.png" alt="blob.png"/></p><p>蝙蝠侠+蝙蝠配饰+蝙蝠宠物<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099719275683599571192.png" title="blob.png" alt="blob.png"/></p><p>小丑鱼，葡萄当泡泡，开心果当石头，创意好棒<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099723155175784756960.png" title="blob.png" alt="blob.png"/></p><p>矮鹈鹕向长腿丹顶鹤求爱，藕片树真是醉了<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099726170703122436988.png" title="blob.png" alt="blob.png"/></p><p>熊妈和熊宝的休憩时光<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099728545703126026080.png" title="blob.png" alt="blob.png"/></p><p>蠢萌大熊猫<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099731897753909564607.png" title="blob.png" alt="blob.png"/></p><p>小水莲和小瓢虫<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099735838964843337229.png" title="blob.png" alt="blob.png"/></p><p>水饺变小鹅，松子变芦苇<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099740462207035029443.png" title="blob.png" alt="blob.png"/></p><p>这位妈妈的创意太赞了！<br/></p><p><img src="/ueditor/net/upload/image/20150303/6356099742061425786784199.jpg" title="关注方法.jpg" alt="关注方法.jpg"/></p>
+     * Date : /Date(1425371823800)/
+     * ImgUrl : /ueditor/net/upload/image/20150303/6356099710908496093909191_243_211.png
+     */
+
+    private String Id;
+    private String Title;
+    private String Content;
+    private String Date;
+    private String ImgUrl;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String Content) {
+        this.Content = Content;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    public String getImgUrl() {
+        return ImgUrl;
+    }
+
+    public void setImgUrl(String ImgUrl) {
+        this.ImgUrl = ImgUrl;
+    }
+}
